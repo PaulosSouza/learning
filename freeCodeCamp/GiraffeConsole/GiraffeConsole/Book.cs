ef57cf22c020b8d0ae334bad946990593cd9a302
Paulo Henrique F de Souza <paulo.souza@apinteractive.com.br>
2021-07-20 16:19:33 -0300

    freeCodeCamp - C# Tutorial - Full Course for Beginners

---
+++ freeCodeCamp/GiraffeConsole/GiraffeConsole/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiraffeConsole
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public int Pages;
+
+        public Book() { }
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            Title = aTitle;
+            Author = aAuthor;
+            Pages = aPages;
+        }
+    }
+}
