ded341f58161123ec854d3ec06b98480028c163f
Paulo Henrique <phferreirasouza106@gmail.com>
2022-06-22 10:38:55 -0300

    feat: add some recursive challenge

---
+++ algorithms-data-structure/challenges/recursive/someRecursive.ts
@@ -0,0 +1,21 @@
+// Write a function called someRecursive which accepts an array and a callback
+// The function returns true if a single value in the array returns true when
+// passed to the callback. Otherwise it returns false.
+
+export function someRecursive(
+  input: number[],
+  cb: (val: number) => boolean
+): boolean {
+  if (!input.length) return false;
+
+  const firstInput = input[0];
+
+  const returnCb = cb(firstInput);
+  if (returnCb) return true;
+
+  const valueReturned = someRecursive(input.splice(1), cb);
+
+  return valueReturned;
+}
+
+console.log(someRecursive([1, 1, 3, 2, 1], (val) => val % 2 === 0));
