a9f1a587321bfce8298687de1e926aa3939b0136
Paulo Henrique <phferreirasouza106@gmail.com>
2022-06-22 10:25:27 -0300

    feat: importing files automatically

--- algorithms-data-structure/src/main.ts
+++ algorithms-data-structure/src/main.ts
@@ -1,2 +1,22 @@
-import "../challenges/recursive/reverse";
-import "../challenges/recursive/isPalindrome";
+import fs from "fs";
+import path from "path";
+
+interface Folders {
+  [key: string]: string;
+}
+
+const challengesFolder: Folders = {
+  recursive: path.join(__dirname, "..", "challenges", "recursive"),
+};
+
+for (const folder in challengesFolder) {
+  const folderPath = challengesFolder[folder];
+
+  const files = fs.readdirSync(folderPath, "utf-8");
+
+  files.forEach((fileName) => {
+    const filePath = path.join(folderPath, fileName);
+
+    require(filePath);
+  });
+}
