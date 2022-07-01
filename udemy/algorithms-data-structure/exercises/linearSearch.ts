0823ab3a0bcffc45aabdaa997f09f2d78b1a38f5
Paulo Henrique Ferreira de Souza <paulo.souza@cubos.io>
2022-07-01 13:10:15 +0000

    feat(algorithms-data-structure): implements linear search

---
+++ udemy/algorithms-data-structure/exercises/linearSearch.ts
@@ -0,0 +1,14 @@
+// write a function called linearSearch which accepts an array and a value,
+// and returns the index at which the value exists. If the value does not exist
+// in the array, return -1
+
+export function linearSearch(input: number[], itemSearchable: number): number {
+  for (let i = 0; i < input.length; i++) {
+    const elem = input[i];
+
+    if (elem === itemSearchable) return i;
+  }
+  return -1;
+}
+
+linearSearch([10, 15, 20, 25, 30], 15);
